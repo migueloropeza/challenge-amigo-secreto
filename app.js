729dc28f3c225cc0f600ef6d5b722073513476b6
mickyWork <mickyoropeza@hotmail.com>
2025-03-12 14:30:13 -0400

    funcionalidades del app.js

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombreAmigo;
+//nombreAmigo = document.getElementById("amigo");
+function agregarAmigo() {
+    nombreAmigo = document.getElementById('amigo');
+    console.log(nombreAmigo);
+    alert(nombreAmigo);
+}
