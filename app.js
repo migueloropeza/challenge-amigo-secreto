211ef7e30408db20fdedf89f1de0bb24a5155075
mickyWork <mickyoropeza@hotmail.com>
2025-03-13 14:22:29 -0400

    app.js con lista de Amigo y probando la funcionalidad de sorteo

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let nombreAmigo;
 //nombreAmigo = document.getElementById("amigo");
+var listaDeAmigos = [];
 function agregarAmigo() {
-    nombreAmigo = document.getElementById('amigo');
-    console.log(nombreAmigo);
-    alert(nombreAmigo);
+    if (document.getElementById('amigo').value != '') {
+        var nombreAmigo = document.getElementById('amigo').value;
+        listaDeAmigos.push(nombreAmigo);
+        document.getElementById('amigo').value = '';
+        document.getElementById('amigo').focus();
+        document.getElementById('listaAmigos').innerHTML += `<li>${listaDeAmigos[listaDeAmigos.length-1]}</li>`;
+        console.log(listaDeAmigos);
+    } else {
+        alert('Debe introducir un nombre correcto.');
+        document.getElementById('amigo').focus();
+    }
+    
+}
+
+function sortearAmigo() {
+    if (!listaDeAmigos.length) {
+        alert('Debe agregar un amigo.')
+        document.getElementById('amigo').focus();
+    } else {
+        let numeroSorteado = Math.random()*listaDeAmigos.length;
+        document.getElementById('resultado').innerHTML = numeroSorteado;
+        console.log(Math.round(Math.random()*listaDeAmigos.length));
+    }
 }
